e2ff729ccfc32a83fe51baf0bf2ab2e7387aff9f
tomzcn <tomz@163.com>
2023-02-10 17:03:24 +0800

    1

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    f=open('1.txt','w')
+    f.write('1')
+    f.close()
+    f=open('1.txt','r')
+    f.read()
+    f.close()
     return 'Hello, World!'
